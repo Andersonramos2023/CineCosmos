9b3e77d6ad77f21de03ae8257dfe3cc2aef4a217
Andersonramos2023 <andersonrsanjo@gmail.com>
2025-11-25 11:58:51 -0300

    modificado

--- src/views/Ficção/Components-Aventura/headerComponentsAventura.vue
+++ src/views/Ficção/Components-Aventura/headerComponentsAventura.vue
@@ -20,6 +20,7 @@ const irParaResultados = () => {
     query: { q: query.value}
   })
 }
+
 const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2RjNWUwYzU1YTNjMDJkMDQ2YmNkMTJlYWI3YTM0OSIsIm5iZiI6MTc1OTUxMjk5MC4yNiwic3ViIjoiNjhlMDA5OWVmZjlkOTllNGNlMTg5MTM4Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.LJTzN7Qih5GOupppGmYJyady3AnjQP42wcIA3XdL2pc";
 
 const searchTv = async () => {
